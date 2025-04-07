c31a3562e02261954867eb31b9fe2b53b057587b
Leo Laaksonen <leo.laaksonen@hotmail.com>
2025-04-07 16:08:17 +0300

    Main opens window that can be closed with ESC

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
 
 #include "../include/miniRT.h"
 
-int32_t main(void)
+void key_input(void *param)
 {
-	ft_printf("miniRT\n");
+	
+	mlx_t *mlx = param;
+	//Closes the windows with escape
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+}
+
+int main(void)
+{
+	mlx_t	*mlx;
+
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "miniRT", true)))
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+
+	//calls key function when key is pressed
+	mlx_loop_hook(mlx, key_input, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+
 	return (0);
 }
