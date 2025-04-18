9fb7e9a7145f8ca3e2f204d6caca509b58968f8f
aaiinnaa22 <aina.elona.albrecht@gmail.com>
2025-04-18 21:24:36 +0300

    rm comment

--- src/raytracing/intersection/intersect_plane.c
+++ src/raytracing/intersection/intersect_plane.c
@@ -6,7 +6,7 @@
 /*   By: aalbrech <aalbrech@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:36:21 by aalbrech          #+#    #+#             */
-/*   Updated: 2025/04/17 17:58:20 by aalbrech         ###   ########.fr       */
+/*   Updated: 2025/04/18 21:20:06 by aalbrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 /*
 Arguments:
-A plane in the scene, and a ray from the camera going in the direction of a certain pixel on the screen. 
+A plane in the scene, and a ray from the camera going in the direction of a certain pixel on the screen.
 
 Description:
 Find out if the ray intersects with the plane in the 3d scene.
-A plane in this case is infinite (it has no specified length or area). 
-A ray can intersect the plane, not intersect the plane, 
-or be perfectly aligned with and lie on the plane (The ray touches an eternal amount of points on the sphere). 
+A plane in this case is infinite (it has no specified length or area).
+A ray can intersect the plane, not intersect the plane,
+or be perfectly aligned with and lie on the plane (The ray touches an eternal amount of points on the sphere).
 
 We have the ray formula P(t) = O + t * D, where
 P(t) = a point on the ray,
@@ -42,12 +42,12 @@ t =  (Po - O) * n / D * n
 
 If D *n is equal to 0, the ray and plane are parallell,
 If D * n = 0 AND (Po - O) * n = 0, the ray is perfectly aligned with and lies on the plan. In this case we
-return 0.0001 as a random point on the ray, to prevent the pixel the ray is 
+return 0.0001 as a random point on the ray, to prevent the pixel the ray is
 sampling from to be missed in the result rendered image. We return such a small value (0.0001 units from the ray origin),
 to make sure we realistically capture the plane in the scene. Since the ray is "hitting" the plane at every point,
-and the plane is infinite, Returning a hit point as soon as we can from the camera is safest. FIX STILL
+and the plane is infinite, Returning a hit point as soon as we can from the camera is safest.
 
-Otherwise we calculate the result t. If t < 0, the intersection is behind the camera. 
+Otherwise we calculate the result t. If t < 0, the intersection is behind the camera.
 
 */
 static float intersect_plane(t_plane *plane, t_ray ray)
